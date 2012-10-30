e39c9cd5662e78c2d0c63c1d4aa90913b82cdcfa
Antonin Delpeuch <antonin@delpeuch.eu>
2012-10-30 18:26:03 +0100

    Ajout de caractères d'échappement.

--- tests/nous/syntax/teststringmechante.c
+++ tests/nous/syntax/teststringmechante.c
@@ -3,6 +3,6 @@ int test ()
     int a;
     int b;
     char* c;
-    c="Ceci est une méchante \" chaine \". C'est pour ça qu'il y a du rouge sous vim";
+    c="Ceci est une méchante \" chaine \". C\'est pour ça qu\'il y a du rouge sous vim";
 }
 
