e53082f9fdc364b38416cdb9eaa0690c299ae6bb
Antonin Delpeuch <antonin@delpeuch.eu>
2012-11-14 19:02:39 +0100

    Ajout d'un fichier oublié : errors.

---
+++ errors.ml
@@ -0,0 +1,23 @@
+
+exception SyntaxError of string
+
+open Ast
+
+let make_label startpos endpos =
+    let sp = startpos.Lexing.pos_bol in
+         {file=(startpos.Lexing.pos_fname);
+          line=(startpos.Lexing.pos_lnum);
+          cbegin=(startpos.Lexing.pos_cnum - sp);
+          cend=(endpos.Lexing.pos_cnum - sp)} 
+
+let string_of_label lb =
+    let cend = (if lb.cbegin < lb.cend then lb.cend else lb.cbegin+1) in
+        Format.sprintf "File %s, line %d, characters %d-%d:\n"
+        lb.file
+        lb.line
+        lb.cbegin
+        cend
+
+
+
+
