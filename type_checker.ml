6d5ebdfbb35a8aef6553833521d7104f81750521
Antonin Delpeuch <antonin@delpeuch.eu>
2012-11-19 20:19:51 +0100

    Ajout de multiples fonctions pour typer. Début de rejet des identifiants non déclarés.

--- type_checker.ml
+++ type_checker.ml
@@ -3,13 +3,24 @@ open Ast
 open Types
 open Errors
 
+(* Cette expression est-elle une valeur gauche ? *)
 let rec is_lvalue = function
     | AE_ident _ -> true
     | AE_star _ -> true
     | AE_dot ((_,e),_) -> is_lvalue e
     | _ -> true
 
-    (* Met à jour l'environnement et renvoie le type *)
+    (* Vérifie le type d'une liste d'éléments avec le typeur fourni *)
+let rec type_list typer env = function
+    | [] -> ()
+    | h::t -> typer env h; type_list typer env t
+
+    (* Renvoie l'environnement mis à jour après lecture de la liste fournie *)
+let rec update_env typer env = function
+    | [] -> env
+    | h::t -> update_env typer (typer env h) t
+
+    (* Renvoie l'arbre étiqueté par ses types *)
 let rec type_expr env (lbl,expr) = match expr with
     (* TODO *)
     | AE_int x -> (ET_int, TE_int x)
@@ -28,21 +39,51 @@ let rec type_expr env (lbl,expr) = match expr with
             (ET_star et, TE_star (et,te))           
     | _ -> (ET_void, TE_int 0) 
 
-
-
-    (* Met à jour l'environnement et renvoie le type *)
-let type_instr env = function
+    (* Renvoie l'arbre étiqueté par ses types *)
+let type_instr env (lbl,instr) = match instr with
     (* TODO *)
     | AI_none -> ET_void
+    | AI_return(Some x) -> fst (type_expr env x)
     | _ -> ET_void
 
-    (* Met à jour l'environnement sans renvoyer de type *)
+    (* Renvoie le type représenté par l'expression de type correspondante *)
+let rec type_type env x = match x with
+    | A_void -> ET_void
+    | A_int -> ET_int
+    | A_char -> ET_char
+    (* TODO : struct & union *)
+    | _ -> ET_void
+
+    (* Renvoie le type et l'identifiant de la variable déclarée *)
+let rec type_and_id_of_avar env basetype = function
+    | AV_ident (_,s) -> (type_type env basetype, s)
+    | AV_star x -> let (t,s) = (type_and_id_of_avar env basetype x) in
+                    (ET_star t, s)
+
+
+    (* Met à jour l'environnement après déclaration de variables *)
+                    (* TODO : remove ldecl_vars *)
+let type_declvar env (lb,((lbl,basetype),lst)) =
+    let add_to env v =
+        let (value,key) = type_and_id_of_avar env basetype v in
+        EnvType.add key value env
+    in
+    List.fold_left add_to env lst
+
+let type_bloc env (lbl,(dvars,dinstr)) =
+        let nenv = update_env type_declvar env dvars in
+        let _ = type_list type_instr nenv dinstr in ()
+
+    (* Met à jour l'environnement sans renvoyer de type,
+     * mais en vérifiant que tout est bien typé *)
 let type_decl env (lbl,decl) = match decl with
     (* TODO *)
     | Adecl_vars _ -> ()
+    | Adecl_fct (ret, nbs, name, args, body) ->
+            type_bloc env body 
     | _ -> ()
 
 let type_ast ast =
-    let env = [] in
+    let env = EnvType.empty in
     List.iter (type_decl env) ast
 
