b59f0d7583fceb4a4e9c300916bcec29a6ddee2f
thomas bourgeat <thomas.07fr@gmail.com>
2012-12-29 17:27:06 +0100

    Peut etre inutile vu ce qui a été fait dans rtl

--- ertl.ml
+++ ertl.ml
@@ -4,8 +4,9 @@ Conventions d'appel: (Avant dernier cours)
 -Résultat renvoyé dans $v0
 -Les registres s sont sauvegardés par l'appelé, les autres par l'appelant.
 *)
+open Rtl
 
-type label
+type label = int
 
 type pseudoreg=
   | Notreg
@@ -13,42 +14,35 @@ type pseudoreg=
   | A0
   | Pseudo of int
 
-type address =
-  | Alab of string
-  | Areg of int * pseudoreg
-
-type operand =
-  | Oimm of int
-  | Oreg of pseudoreg
 
 type instr=
   | Ecall of ident*int*label
   | Esyscall of label
   | Ealloc_frame of label
   | Edelete_frame of label
-  | Eget_stack_param of register*int*label
-  | Eset_stack_param of register*int*label
+  | Eget_stack_param of pseudoreg*int*label
+  | Eset_stack_param of pseudoreg*int*label
   | Ereturn
-  | Emunop of register*munop*register*label
-  | Embinop of register*mbinop*register*label
-  | Econst of register*int*label
+  | Econst of pseudoreg*int*label
 (*Suite ne change pas de précedemment*)
   | Emove of pseudoreg*pseudoreg*label
-  | Li   of pseudoreg * int32 * label
-  | La   of pseudoreg * label * label
-  | Lw   of pseudoreg * address * label
-  | Sw   of pseudoreg * address * label
-  | Arith of Mips.arith * pseudoreg * pseudoreg * operand * label
-  | Neg  of pseudoreg * pseudoreg * label
+  | ELi   of pseudoreg * int32 * label
+  | ELa   of pseudoreg * label * label
+  | ELw   of pseudoreg * address * label
+  | ESw   of pseudoreg * address * label
+  | EArith of Mips.arith * pseudoreg * pseudoreg * operand * label
+  | ENeg  of pseudoreg * pseudoreg * label
 (*| Set *)
-  | B    of label
-  | Beq  of pseudoreg * pseudoreg * label
-  | Beqz of pseudoreg * label
-  | Bnez of pseudoreg * label
-  | Jr   of pseudoreg
-  | Syscall of label
+  | EB    of label
+  | EBeq  of pseudoreg * pseudoreg * label
+  | EBeqz of pseudoreg * label
+  | EBnez of pseudoreg * label
+  | EJr   of pseudoreg
+  | ESyscall of label
 
+module M = Map.Make(struct type t=label
+      let compare=compare end)
  
-type graph
+type graph= instr M.t
 
 
