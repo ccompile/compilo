1e9dad2fa9f020f8dbeb6791f3ca8c8b1cf78eb9
thomas bourgeat <thomas.07fr@gmail.com>
2013-01-01 21:10:17 +0100

    Fix sbrk and Eprintf, not verified

--- ertl.ml
+++ ertl.ml
@@ -39,10 +39,9 @@ type instr=
   | EBnez of pseudoreg * label
   | EJr   of pseudoreg
 
-module M = Map.Make(struct type t=label
-      let compare=compare end)
  
-type graph= instr M.t
+
+
 let move src dst l = generate (Emunop (dst, Mmove, src, l))
 let set stack r n l = generate (Eset stack param (r, n, l))
 
@@ -53,9 +52,39 @@ let compil_instr = function
     let l = generate (Ecall (x, n, move Register.result r l)) in
     let ofs = ref 0 in
     let l = List.fold left
-       (fun l t -> ofs := !ofs - word size; set stack t !ofs l)
+       (fun l t -> ofs := !ofs - 4; set stack t !ofs l)
        l fsl
     in
     let l = List.fold right (fun (t, r) l -> move t r l) frl l in
     Egoto l
+  | Rtl.Putchar(r,bidon, l) ->
+    Econst (Register.v0, 1, (
+    move r Register.a0 (generate (
+    Esyscall (generate (
+    Econst (Register.a0, 10, generate (
+    Econst (Register.v0, 11, generate (
+    Esyscall l))))))))))
+  | Rtl.sbrk (r, n, l) ->
+    Econst (Register.a0, n, generate (
+    Econst (Register.v0, 9, generate (
+    Esyscall (
+    move Register.v0 r l)))))
+
+
+let fun entry savers formals entry =
+  let frl, fsl = assoc formals formals in
+  let ofs = ref 0 in
+  let l = List.fold left
+    (fun l t -> ofs := !ofs - word size; get stack t !ofs l)
+    entry fsl
+  in
+  let l = List.fold right (fun (t, r) l -> move r t l) frl l in
+  let l = List.fold right (fun (t, r) l -> move r t l) savers l in
+  generate (Ealloc frame l)
+
+let fun exit savers retr exitl =
+  let l = generate (Edelete frame (generate Ereturn)) in
+  let l = List.fold right (fun (t, r) l -> move t r l) savers l in
+  let l = move retr Register.result l in
+  graph := Label.M.add exitl (Egoto l) !graph
  
