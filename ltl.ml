5f05be1d220860f6a5f03183cc7c2350a1d836c7
thomas bourgeat <thomas.07fr@gmail.com>
2013-01-04 15:29:16 +0100

    Commentaire à propos d'un problème

--- ltl.ml
+++ ltl.ml
@@ -8,4 +8,7 @@ module M = Map.Make(struct type t=register let compare= compare end)
 type arcs = { prefs : Register.set; intfs : Register.set }
 type graph = arcs M.t
 
-
+let make f =
+  let Fct(name,arg,graph,lab,set,entreesortie) = f in
+  (* TODO : Parcourir le graphe ERTL slide 11 dernier cours
+  ISSUE: Comment éviter les boucles, faire une table dejavu?*)  
