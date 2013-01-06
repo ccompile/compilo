169be8bfede0e15d4381019156fc445dc2801dd5
thomas bourgeat <thomas.07fr@gmail.com>
2013-01-06 01:40:42 +0100

    Fonction principale instr terminée, compile, pas encore testée

--- ltl.ml
+++ ltl.ml
@@ -1,6 +1,7 @@
 open Ertl
 open Register
 open Rtl
+open Irc
 
 type instr=
   | Lcall of string*int*label
@@ -20,7 +21,6 @@ type instr=
   | LAddress of register * int * register * label
   | LArith of Mips.arith * register * register * operand * label
   | LSet of Mips.condition * register* register* operand* label
-  | LNeg of register * register* label
   | Lgoto   of label
   | LBeq  of register * register * label * label
   | LBeqz of register * label * label
@@ -28,42 +28,141 @@ type instr=
   | LJr   of register
   | LReturn  
 
-let tmp1, tmp2 = "$v1", "$fp"
+module M = Map.Make(struct type t=label
+    let compare = compare end)
 
-let write c r l = match lookup c r with
+
+type graph = instr M.t
+
+let graph = ref M.empty
+
+let reset_graph () =
+    graph := M.empty
+
+let generate instr =
+    let lbl = fresh_label () in
+    graph := M.add lbl instr !graph;
+    lbl
+
+let add_instr lbl instr =
+    graph := M.add lbl instr !graph
+
+let find_instr g lbl =
+    M.find lbl g
+
+let iter_instr g fct =
+    M.iter fct g
+
+
+let tmp1, tmp2 = V1, T7
+let lookup c r=  Reg(A0)
+
+let write1 c r l = match lookup c r with
   | Reg hr -> hr, l
-  | Spilled n -> tmp1, generate (Eset_stack (tmp1, n, l))
+  | Stack n -> tmp1, generate (Lset_stack (tmp1, n, l))
 
 let read1 c r f = match lookup c r with
   | Reg hr -> f hr
-  | Spilled n -> Eget_stack (tmp1, n, generate (f tmp1))
+  | Stack n -> Lget_stack (tmp1, n, generate (f tmp1))
 
-let instr c frame size = function
-  | Ertl.Econst (r, n, l) ->
-      let hwr, l = write c r l in
+let write2 c r l = match lookup c r with
+  | Reg hr -> hr, l
+  | Stack n -> tmp2, generate (Lset_stack (tmp2, n, l))
+
+let read2 c r f = match lookup c r with
+  | Reg hr -> f hr
+  | Stack n -> Lget_stack (tmp2, n, generate (f tmp2))
+
+
+let instr c frame_size = function
+(*REGROUPEMENT en factorisation possibles futures facilement*)
+
+(*  | Ertl.Econst (r, n, l) ->
+      let hwr, l = write1 c r l in
       Econst (hwr, n, l)
-  | Ertl.Eaccess_global (r, x, l) ->
-      let hwr, l = write c r l in
-      Eaccess_global (hwr, x, l)
-  | Ertl.Embinop (r1, op, r2, r3, l) ->
-    read2 c r2 r3 (fun hw2 hw3 ->
-    let hw1, l = write c r1 l in
-    Embinop (hw1, op, hw2, hw3, l))
+*)
+
+  | Ertl.ELi(r1,i,l) ->let hw,l=write1 c r1 l in LLi(hw,i,l)
+  | Ertl.ELw(r,s,l)->let hw,l =write1 c r l in LLw(hw,s,l)
+  | Ertl.ELb(r,ad,l)->let hw,l = write1 c r l in LLb(hw,ad,l)
+
+  | Ertl.ESb(r,ad,l)->read1 c r (fun x-> LSb(x,ad,l))
+  | Ertl.ESw(r,ad,l)->read1 c r (fun x-> LSw(x,ad,l))
+  | Ertl.EStr(r1,s,l) ->read1 c r1 (fun x->LStr(x,s,l))
+  | Ertl.EBeqz(r,l1,l2)->read1 c r (fun x->LBeqz(x,l1,l2))
+  | Ertl.EBnez(r,l1,l2)->read1 c r (fun x->LBnez(x,l1,l2))
+  | Ertl.EJr(r)->read1 c r (fun x->LJr(x))
+ 
+
+
+  | Ertl.EAddress(r1,i,r2,l)->
+(*TODO A QUOI SERT CE BIDULE :p j'ai mis n'imp vu que jsavaispas*)
+  LAddress(A0,i,A0,l)
+
+  | Ertl.EReturn-> LReturn
+  | Ertl.Egoto(l)-> Lgoto(l)
+  | Ertl.Ecall(s,i,l)-> Lcall(s,i,l)
+  | Ertl.Esyscall(l)->Lsyscall(l)
+
+  | Ertl.Emove(r1,r2,l)->
+           let (hw1,l)=write2 c r1 l in 
+                        read1 c r2 (fun x-> Lmove(hw1,x,l)) 
+  
+
+  | Ertl.EBeq(r1,r2,l1,l2)->
+        read1 c r2 (fun x-> read2 c r1 (fun y-> LBeq(y,x,l1,l2)))  
+
+ 
+ 
+  | Ertl.ESet(op,r2,r3,operand,l)->
+  begin
+      match operand with 
+        |Rtl.Oimm(a)->
+          read1 c r3 
+                (fun x-> let (hw1,l)= write1 c r2 l in
+                      LSet(op,hw1,x,Oimm(a),l)
+                )
+        |Rtl.Oreg(b)-> 
+          let f tmp regist= let (hw1,l)=write1 c r2 l in
+          LSet(op,hw1,tmp,Oreg(regist),l) 
+          in 
+          read1 c r3 (fun x-> (read2 c b (fun y-> f x y)))    
+      end
+  | Ertl.EArith ( op, r2, r3,operand, l) ->
+  begin
+      match operand with 
+        |Oimm(a)->
+          read1 c r3 
+                (fun x-> let (hw1,l)= write1 c r2 l in
+                      LArith(op,hw1,x,Oimm(a),l)
+                )
+        |Oreg(b)-> 
+          let f tmp regist= let (hw1,l)=write1 c r2 l in
+          LArith(op,hw1,tmp,Oreg(regist),l) 
+          in 
+          read1 c r3 (fun x-> (read2 c b (fun y-> f x y)))    
+      end
+
+
   | Ertl.Eget_stack_param (r, n, l) ->
-    let hwr, l = write c r l in
-    Eget_stack (hwr, frame_size + n, l)
-  | Ertl.Eset stack_param (r, n, l) ->
-    read1 c r (fun hwr -> Eset stack (hwr, n, l))
+    let hwr, l = write1 c r l in
+    Lget_stack (hwr, frame_size + n, l)
+
+  | Ertl.Eset_stack_param (r, n, l) ->
+    read1 c r (fun hwr -> Lset_stack (hwr, n, l))
+ 
   | Ertl.Ealloc_frame l
   | Ertl.Edelete_frame l when frame_size = 0 ->
-    Egoto l 
+    Lgoto l
+ 
   | Ertl.Ealloc_frame l ->
-    Emunop (Register.sp, Maddi (-frame_size), Register.sp, l)
+ LArith(Mips.Add, Register.sp, Register.sp,Oimm(Int32.of_int(-frame_size)), l)
+
   | Ertl.Edelete_frame l ->
-    Emunop (Register.sp, Maddi frame_size, Register.sp, l)
+ LArith(Mips.Add, Register.sp, Register.sp,Oimm(Int32.of_int(frame_size)), l)
 
-
-
+  |_->assert(false)
+(*
 let deffun t =
   (*let ln = Liveness.analyze f.Ertl.fun_body in
   let ig = Interference.make ln in
@@ -81,5 +180,4 @@ let deffun t =
   { fun name = f.Ertl.fun_name;
     fun entry = f.Ertl.fun_entry;
     fun body = !graph; }
-
-
+*)
